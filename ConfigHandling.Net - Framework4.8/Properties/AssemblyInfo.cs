7aae4bb52c041f2ba158f009b97247e44fed1188
Daan Verstraten <Huntondoom@gmail.com>
2020-02-14 16:21:24 +0100

    Added Nuget

--- ConfigHandling.Net - Framework4.8/Properties/AssemblyInfo.cs
+++ ConfigHandling.Net - Framework4.8/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Config-Handling.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ConfigHandling.Net")]
+[assembly: AssemblyDescription("ConfigHandling.Net takes care of all your config or options objects. It takes care of saving, loading, and persistence of objects in memory. Config objects are saved in a centralized location.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Config-Handling.Net")]
+[assembly: AssemblyCompany("Daan Verstraten")]
+[assembly: AssemblyProduct("ConfigHandling.Net")]
 [assembly: AssemblyCopyright("Copyright © Daan Verstraten 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("77383926-016e-4e81-bbd4-724bd6e6ffa8")]
+[assembly: Guid("9b91c88f-52ff-42a9-83b6-2aed5d94249c")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.7.0")]
+[assembly: AssemblyFileVersion("1.0.7.0")]
