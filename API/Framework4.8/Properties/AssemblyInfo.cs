b013500580667c9d596df5d59dd633c910e3071d
Daan Verstraten <Huntondoom@gmail.com>
2020-05-02 16:45:29 +0200

    Updated

--- API/Framework4.8/Properties/AssemblyInfo.cs
+++ API/Framework4.8/Properties/AssemblyInfo.cs
@@ -1,13 +1,33 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConfigHandling.Net")]
-[assembly: AssemblyDescription("ConfigHandling.Net takes care of all your config or options objects. It takes care of saving, loading, and persistence of objects in memory. Config objects are saved in a centralized location.")]
-[assembly: AssemblyConfiguration("")]
+
+#if X64
+[assembly: AssemblyTitle("ConfigHandling.Net X64")]
+[assembly: AssemblyDescription("ConfigHandling.Net x64 takes care of all your config or options objects. It takes care of saving, loading, and persistence of objects in memory. Config objects are saved in a centralized location.")]
+#else
+#if AnyCpu
+[assembly: AssemblyTitle("ConfigHandling.Net AnyCpu")]
+[assembly: AssemblyDescription("ConfigHandling.Net AnyCpu takes care of all your config or options objects. It takes care of saving, loading, and persistence of objects in memory. Config objects are saved in a centralized location.")]
+#else
+[assembly: AssemblyTitle("ConfigHandling.Net x32")]
+[assembly: AssemblyDescription("ConfigHandling.Net x32 takes care of all your config or options objects. It takes care of saving, loading, and persistence of objects in memory. Config objects are saved in a centralized location.")]
+#endif
+#endif
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+    [assembly: AssemblyConfiguration("RELEASE")]
+#endif
+
+//[assembly: AssemblyTitle("ConfigHandling.Net")]
+//[assembly: AssemblyDescription("ConfigHandling.Net takes care of all your config or options objects. It takes care of saving, loading, and persistence of objects in memory. Config objects are saved in a centralized location.")]
 [assembly: AssemblyCompany("Daan Verstraten")]
 [assembly: AssemblyProduct("ConfigHandling.Net")]
 [assembly: AssemblyCopyright("Copyright © Daan Verstraten 2018")]
@@ -32,5 +52,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.9.0")]
-[assembly: AssemblyFileVersion("1.0.9.0")]
+[assembly: AssemblyVersion("1.0.10.0")]
+[assembly: AssemblyFileVersion("1.0.10.0")]
